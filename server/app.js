fd98216e785164679506fba5a1d9901ba71a8352
leonwgc <guochao.wang@dianrong.com>
2016-03-09 11:00:19 +0800

    refactor

--- server/app.js
+++ server/app.js
@@ -1,21 +1,22 @@
 var koa = require('koa');
 var serve = require('koa-static');
 var favicon = require('koa-favicon');
-var error=require('koa-error');
-var responseTime=require('koa-response-time');
+var error = require('koa-error');
+var responseTime = require('koa-response-time');
 var app = koa();
+var path = require('path');
 
 app.use(responseTime());
 
 // favicon
-app.use(favicon('../pages/favicon.ico'));
+app.use(favicon(path.join(__dirname, '../pages/favicon.ico')));
 
-app.use(serve('../'));
+app.use(serve(path.join(__dirname, '../')));
 
-app.use(error({template:'../pages/error.html'}));
+app.use(error({ template: path.join(__dirname, '../pages/error.html') }));
 
 
-var port=3001;
+var port = 3001;
 app.listen(port);
 
-console.log('app is running at port '+port);
+console.log('app is running at port ' + port);
