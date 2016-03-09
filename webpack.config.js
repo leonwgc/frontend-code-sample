62ab50626370711671c874796a7b58c4c3309e01
leonwgc <guochao.wang@dianrong.com>
2016-03-09 12:50:37 +0800

    add react devtools and react demo with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,33 @@ module.exports = {
   context: context,
   entry: {
     index: './index',
-    index2: './index2'
+    index2: './index2',
+    r1:'./r1',
+    r2:'./r2',
   },
   output: {
     path: buildPath,
     filename: "[name].js"
   },
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel', 
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
   plugins: [
-    // new CommonsChunkPlugin({
-    //   name: "vendor"
-    // })
+    new CommonsChunkPlugin({
+      name: "reactlib",
+      chunks: ["r1", "r2"],
+    })
   ]
 };
